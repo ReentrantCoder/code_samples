4f0254fe724cac94ac657298798d05f6f5c3609c
ReentrantCoder <tylerwbehm@gmail.com>
2016-12-03 16:09:24 -0700

    Add constructor

--- groovy_SQL/fastestPokemon.groovy
+++ groovy_SQL/fastestPokemon.groovy
@@ -6,50 +6,56 @@ sql = Sql.newInstance( 'jdbc:sqlite:pokedex.sqlite', 'org.sqlite.JDBC' )
 types = sql.rows('select identifier, id from types limit 17')
 fastestPokemon = []
 types.each { type ->
-    Pokemon pokemon = new Pokemon()
-    id = idFastestByType( type )
-    pokemon.name  = idToName( id ).capitalize()
-    pokemon.type  = type.identifier
-    pokemon.speed = idToSpeed( id )
+    int id = idFastestByType( type.id )
+    Pokemon pokemon = new Pokemon( id, type.identifier, sql )
     fastestPokemon.add(pokemon)
 }
 fastestPokemon.sort{ it.speed }
 fastestPokemon.each{ println( "${it.name} is the fastest ${it.type}-type pokemon. Speed ${it.speed}" ) }
 
-class Pokemon {
-    String name, type
-    int speed
-}
-
-def idFastestByType( type ) {
+int idFastestByType( int typeID ) {
     id = sql.firstRow(
         """
         select pokemon_id, base_stat from (
         select * from pokemon_stats
         inner join pokemon_types
         on pokemon_stats.pokemon_id = pokemon_types.pokemon_id)
-        where type_id = ${type.id} and stat_id = 6
+        where type_id = ${typeID} and stat_id = 6
         order by base_stat desc
         """)
     id.pokemon_id
 }
 
-def idToName( id ) {
-    def name = sql.firstRow(
-        """
-        select identifier from pokemon
-        inner join pokemon_species
-        on pokemon.species_id = pokemon_species.id
-        where pokemon.id = ${id}
-        """)
-    name.identifier
+class Pokemon {
+    String name, type
+    Integer speed
+
+    Pokemon(Integer id, String type, Sql sql) {
+        this.name = idToName ( id, sql )
+        this.type = type
+        this.speed = idToSpeed ( id, sql)
+
+
+    }
+
+    static String idToName( Integer id, Sql sql ) {
+        def name = sql.firstRow(
+            """
+            select identifier from pokemon
+            inner join pokemon_species
+            on pokemon.species_id = pokemon_species.id
+            where pokemon.id = ${id}
+            """)
+        name.identifier
+    }
+
+    static Integer idToSpeed( Integer id, Sql sql ) {
+        def speed = sql.firstRow(
+            """
+            select base_stat from pokemon_stats
+            where pokemon_id = ${id} and stat_id = 6
+            """)
+        speed.base_stat
+    }
 }
 
-def idToSpeed( id ) {
-    def speed = sql.firstRow(
-            """
-        select base_stat from pokemon_stats
-        where pokemon_id = ${id} and stat_id = 6
-        """)
-    speed.base_stat
-}
