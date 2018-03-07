07616f8999ad851028a1b52e12b88ad93ed0134c
ReentrantCoder <tylerwbehm@gmail.com>
2018-03-07 17:44:12 -0600

    Fix whitespace/indent

--- BFSdiameter_Java/Node.java
+++ BFSdiameter_Java/Node.java
@@ -1,13 +1,13 @@
 
-public class Node{
-        //0=white, 1=gray, 2=black
-		public int color;
-		public int distance;
-		public int predecessor;
-		
-		public Node(){
-		this.color = 0;
-		this.distance = -1;
-		this.predecessor = -1;
-	}
+public class Node {
+    //0=white, 1=gray, 2=black
+    public int color;
+    public int distance;
+    public int predecessor;
+
+    public Node() {
+        this.color = 0;
+        this.distance = -1;
+        this.predecessor = -1;
+    }
 }
